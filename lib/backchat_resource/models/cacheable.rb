41805121f629ceb0ea2aa57cacceceb993aadbd8
Adam Burmister <adam.burmister@gmail.com>
2011-04-03 10:22:29 +0100

    Adding pending specs for URI

---
+++ lib/backchat_resource/models/cacheable.rb
@@ -0,0 +1,5 @@
+module BackchatResource
+  module Cacheable
+    
+  end
+end
--- spec/backchat_uri_spec.rb
+++ spec/backchat_uri_spec.rb
@@ -3,186 +3,68 @@ require 'backchat_resource/backchat_uri'
 
 describe "BackchatResource::BackchatUri" do
 
-  
-  it "can expand a uri" do
+  it "should parse a uri" do
+    uri = "twitter://backchatio#timeline"
     pending
   end
   
+  it "should parse a uri without a kind" do
+    uri = "twitter://backchatio"
+    pending
+  end
   
-  # # The blank shape to be merged with expected results
-  # parts_shape = {
-  #   :scheme=>nil,
-  #   :user=>nil,
-  #   :password=>nil,
-  #   :source=>nil,
-  #   :port=>nil,
-  #   :resource=>nil,
-  #   :resources=>nil,
-  #   :querystring=>nil,
-  #   :query=>nil,
-  #   :kind=>nil,
-  #   :resource_kind=>nil
-  # }
-  # 
-  # context "convert from a uri" do
-  # 
-  #   it "should convert from a Uri without a kind" do
-  #     uri = "twitter://casualjim"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     # puts bcuri.inspect
-  #     # puts bcuri.parts.inspect
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "twitter",
-  #      :source => "casualjim"
-  #     })
-  #   end
-  # 
-  #   it "should convert from a Uri with a channel kind" do
-  #     uri = "twitter://casualjim#trends"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "twitter",
-  #      :source => "casualjim",
-  #      :kind => "trends"
-  #     })
-  #   end
-  # 
-  #   it "should convert from a Uri with a channel kind" do
-  #     uri = "twitter://casualjim#trends"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "twitter",
-  #      :source => "casualjim",
-  #      :kind => "trends"
-  #     })
-  #   end
-  # 
-  #   it "should convert from a channel with escaped hash" do
-  #     uri = "twitter://casualjim%23timeline"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "twitter",
-  #      :source => "casualjim",
-  #      :kind => "timeline"
-  #     })      
-  #   end
-  # 
-  #   it "convert from a Uri with a channel kind and channel resource" do
-  #     uri = "twitter://casualjim#timeline/yesterday"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "twitter",
-  #      :source => "casualjim",
-  #      :kind => "timeline",
-  #      :resource_kind => "yesterday"
-  #     })
-  #   end
-  # 
-  #   it "should parse an xmpp uri" do
-  #     uri = "xmpp://adam@mojolly.com:pass123@xmpp.mojolly.com:3000/mymacbook#muc/myroom"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "xmpp",
-  #      :user => "adam@mojolly.com",
-  #      :password => "pass123",
-  #      :source => "xmpp.mojolly.com",
-  #      :port => 3000,
-  #      :resource => "mymacbook",
-  #      :resources => ["mymacbook"],
-  #      :kind => "muc",
-  #      :resource_kind => "myroom"
-  #     })
-  #   end
-  # 
-  #   it "should parse an encoded xmpp uri" do
-  #     uri = "xmpp%3A%2F%2Fadam%40mojolly.com%3Apass123%40xmpp.mojolly.com%3A3000%2Fmymacbook%23muc%2Fmyroom"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "xmpp",
-  #      :user => "adam@mojolly.com",
-  #      :password => "pass123",
-  #      :source => "xmpp.mojolly.com",
-  #      :port => 3000,
-  #      :resource => "mymacbook",
-  #      :resources => ["mymacbook"],
-  #      :kind => "muc",
-  #      :resource_kind => "myroom"
-  #     })
-  #   end
-  # 
-  #   it "should convert from a Uri with a querystring" do
-  #     uri = "twitter://casualjim?bql=text%20has%20something"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "twitter",
-  #      :source => "casualjim",
-  #      :querystring => "bql=text%20has%20something",
-  #      :query => {
-  #        "bql" => "text has something"
-  #      }
-  #     })
-  #   end
-  # 
-  #   it "should convert from a Uri with a querystring with multi params" do
-  #     uri = "twitter://casualjim?bql=text%20has%20something&something=else"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "twitter",
-  #      :source => "casualjim",
-  #      :querystring => "bql=text%20has%20something&something=else",
-  #      :query => {
-  #        "bql" => "text has something",
-  #        "something" => "else"
-  #      }
-  #     })
-  #   end
-  # 
-  #   it "should convert from a Uri with a kind and querystring with multi params" do
-  #     uri = "twitter://casualjim?bql=text%20has%20something&something=else#timeline"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "twitter",
-  #      :source => "casualjim",
-  #      :querystring => "bql=text%20has%20something&something=else",
-  #      :query => {
-  #        "bql" => "text has something",
-  #        "something" => "else"
-  #      },
-  #      :kind => "timeline"
-  #     })
-  #   end
-  # 
-  #   it "should convert from a Uri with a fragment and fragment resource" do
-  #     uri = "xmpp://ivan@mojolly.com:password123@xmpp.mojolly.com/adium#muc/room"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #      :scheme => "xmpp",       
-  #      :user => "ivan@mojolly.com",
-  #      :password => "password123",
-  #      :source => "xmpp.mojolly.com",
-  #      :resource => "adium",
-  #      :resources => ["adium"],
-  #      :kind => "muc",
-  #      :resource_kind => "room"
-  #     })
-  #   end
-  # 
-  #   it "should convert from a Uri with alternative credential passing" do
-  #     uri = "twitter://backchatio?user=ivan@mojolly.com&pass=password123"
-  #     bcuri = BackchatResource::BackchatUri.parse(uri)
-  #     bcuri.parts.should == parts_shape.merge({
-  #       :scheme => "twitter",       
-  #       :source => "backchatio",
-  #       :querystring => "user=ivan@mojolly.com&pass=password123",
-  #       :query => {
-  #         "user" => "ivan@mojolly.com",
-  #         "pass" => "password123"
-  #       },
-  #       :user => "ivan@mojolly.com",
-  #       :password => "password123"
-  #     })
-  #   end
+  it "should parse an escaped URI" do
+    uri = "twitter://backchatio%23timeline"
+    pending
+  end
   
-  # endnd
+  it "should parse a URI with a querystring param but no kind" do
+    uri = "twitter://backchatio?bql=text%20has%20something"
+    pending
+  end
+
+  it "should parse a URI with a querystring param and a kind" do
+    uri = "twitter://backchatio?bql=text%20has%20something#timeline"
+    pending
+  end
+    
+  it "should parse a Uri with a querystring with multi params" do
+    uri = "twitter://backchatio?bql=text%20has%20something&something=else#timeline"
+    pending
+  end  
+  
+  it "should parse a URI with a kind and kind_resource" do
+    uri = "tel://080012345678#sms/hashblue"
+    pending
+  end
+
+  it "should parse a Uri with credential" do
+    uri = "rss://basicauthuser:mypass123@server.com/secure/feed.xml"
+    pending
+  end
+  
+  it "parsed URIs have a source model instance" do
+    pending
+  end
+  
+  it "parsed URIs have a kind model instance" do
+    pending
+  end
+  
+  it "has a kind_resource attribute accessor" do
+    pending
+  end
+  
+  it "has a params hash containg querystring params" do
+    pending
+  end
+  
+  it "can set a querystring param and have it rendered as a string" do
+    pending
+  end
+
+  it "can set a querystring param, and not clobber any other existing params, and have it rendered as a string" do
+    pending
+  end
 
 end
