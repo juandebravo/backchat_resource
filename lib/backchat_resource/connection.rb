1fc6bea8cabf8515699b4643b8c1e05667e8a104
Adam Burmister <adam.burmister@gmail.com>
2011-04-19 16:02:35 +0100

    Adding gemspec to see if it fixes issues with deploy

--- lib/backchat_resource/connection.rb
+++ lib/backchat_resource/connection.rb
@@ -14,37 +14,6 @@ module ActiveResource
     @@cache = Cache.new :expiration => ShortCacheTimeout
     
     private
-    
-      # def handle_response(response)
-      #   case response.code.to_i
-      #     when 301,302
-      #       raise(Redirection.new(response))
-      #     when 200...400
-      #       response
-      #     when 400
-      #       raise(BadRequest.new(response))
-      #     when 401
-      #       raise(UnauthorizedAccess.new(response))
-      #     when 403
-      #       raise(ForbiddenAccess.new(response))
-      #     when 404
-      #       raise(ResourceNotFound.new(response))
-      #     when 405
-      #       raise(MethodNotAllowed.new(response))
-      #     when 409
-      #       raise(ResourceConflict.new(response))
-      #     when 410
-      #       raise(ResourceGone.new(response))
-      #     when 422
-      #       raise(ResourceInvalid.new(response))
-      #     when 401...500
-      #       raise(ClientError.new(response))
-      #     when 500...600
-      #       raise(ServerError.new(response))
-      #     else
-      #       raise(ConnectionError.new(response, "Unknown response code: #{response.code}"))
-      #   end
-      # end
       
       alias_method :old_request, :request
       # Makes a request to the remote service.
