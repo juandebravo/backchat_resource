abb54d928564c3b0c8996248119e47db9607e958
Adam Burmister <adam.burmister@gmail.com>
2011-05-16 22:03:15 +0100

    Adding logic to marshal target into parts for editing

--- lib/backchat_resource/models/channels/email_dropbox.rb
+++ lib/backchat_resource/models/channels/email_dropbox.rb
@@ -3,7 +3,7 @@ module BackchatResource
     module Channels
       class EmailDropbox < BackchatResource::Models::Channels::Generic
         
-        attr_accessor :whitelist
+        attr_accessor :whitelist, :user_login
         
         HUMANIZED_ATTRIBUTES = {
           :target => "Dropbox email address"
@@ -12,14 +12,25 @@ module BackchatResource
           HUMANIZED_ATTRIBUTES[params.to_sym] || super(params, options)
         end
 
-        validates :target, :presence => true, :format => { :with => /^[a-zA-Z0-9\-+_]+$/ }
+        # validates :target, :presence => true, :format => { :with => /^[a-zA-Z0-9\-+_].[a-zA-Z0-9\-+_]+$/ }
         
         def display_target
           "#{target}@backchat.io"
         end
         
+        # Add in the user_login name
         def target=(val)
+          # if val[0..user_login.length] != "#{user_login}."
+          val = "#{user_login}.#{val}"
+          # end
+          
           super(val.to_slug)
+        end
+        
+        # Strip out the user_login name from the target for editing
+        def target
+          tgt = super
+          tgt.gsub(/^.*\./, '') if !tgt.nil?
         end
 
         def whitelist
@@ -35,7 +46,6 @@ module BackchatResource
         def whitelist=(lst)
           uri.querystring_params["whitelist"] = lst.split(",").map(&:strip).reject{|e|e.blank?}
         end
-   
         
       end
     end
